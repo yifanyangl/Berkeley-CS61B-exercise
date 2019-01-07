d12d57433c8c79d47659a229319ae241cf0fd34a
Kai Liao <38992157+yifanyangl@users.noreply.github.com>
2019-01-06 19:19:12 -0500

    [lec05] Skips A-level exercise 1

---
+++ src/main/java/lec05/Piece.java
@@ -0,0 +1,24 @@
+/*
+A-Level Exercise 1, problem 10 from practice midterm 1 in Kartik’s textbook
+Can't understand the question, passed.
+ */
+package lec05;
+
+public class Piece {
+
+    private int longtitude;
+    private int latitude;
+
+    public Piece(int lng, int lat) {
+        longtitude = lng;
+        latitude = lat;
+    }
+
+    public Piece[][] sortByLat(Piece[] p) {
+        return null;
+    }
+
+    private Piece[][] sortLatitudes(Piece[][] unsorted) {
+        return null;
+    }
+}
