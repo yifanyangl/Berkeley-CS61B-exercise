f85cbe3b99f4d395946d3c318c9ea24a2bd09ac9
Kai Liao <38992157+yifanyangl@users.noreply.github.com>
2019-01-09 14:30:21 -0500

    [lec06] Completes discussion 3 exercise 2.2

--- src/main/java/lec06/Discussion3Arrays.java
+++ src/main/java/lec06/Discussion3Arrays.java
@@ -11,6 +11,14 @@ Is it possible to write a version of this method that returns void and changes a
 (i.e., destructively)?
 Not possible. Array is re-created and the reference value passed as parameter can not be
 changed.
+
+
+2.2
+Consider a method that destructively reverses the items in arr.
+For example calling reverse on an array [1, 2, 3] should change the array to be [3, 2, 1].
+
+What is the fewest number of iteration steps you need? What is the fewest number of additional
+variables you need?
  */
 package lec06;
 
@@ -28,6 +36,14 @@ public class Discussion3Arrays {
         newArr[position] = item;
         System.arraycopy(arr, position, newArr, position + 1, arr.length - position);
         return newArr;
+    }
+
+    public static void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
     }
 
 }
--- src/test/java/lec06/Discussion3ArraysTest.java
+++ src/test/java/lec06/Discussion3ArraysTest.java
@@ -1,6 +1,7 @@
 package lec06;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -39,5 +40,29 @@ public class Discussion3ArraysTest {
         int[] arr = new int[]{};
         int[] got = Discussion3Arrays.insert(arr, 7, 0);
         assertArrayEquals(new int[]{7}, got);
+    }
+
+    @Test
+    public void reverse() {
+        Discussion3Arrays.reverse(testArr);
+        assertArrayEquals(new int[]{15, 14, 9, 5}, testArr);
+    }
+
+    @Test
+    public void reverseEmptyArray() {
+        int[] arr = new int[]{};
+        Discussion3Arrays.reverse(arr);
+        assertEquals(0, arr.length);
+    }
+
+    @Test
+    public void reverseArrayOfOddLength() {
+        int[] arr = new int[]{1, 2, 3};
+        Discussion3Arrays.reverse(arr);
+        assertArrayEquals(new int[]{3, 2, 1}, arr);
+
+        arr = new int[]{1};
+        Discussion3Arrays.reverse(arr);
+        assertArrayEquals(new int[]{1}, arr);
     }
 }
